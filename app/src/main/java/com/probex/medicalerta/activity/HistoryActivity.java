35028855b8ea8d8ddcee412a10f02baa201c7ecd
Lucas Barbosa <lucastssb@gmail.com>
2020-09-20 10:36:21 -0300

    Made some improvements on alarm list screen and added material design

---
+++ app/src/main/java/com/probex/medicalerta/activity/HistoryActivity.java
@@ -0,0 +1,16 @@
+package com.probex.medicalerta.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.probex.medicalerta.R;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
--- app/src/main/java/com/probex/medicalerta/adapter/AlarmItemAdapter.java
+++ app/src/main/java/com/probex/medicalerta/adapter/AlarmItemAdapter.java
@@ -1,5 +1,6 @@
 package com.probex.medicalerta.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -7,32 +8,47 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.probex.medicalerta.R;
+
 public class AlarmItemAdapter extends RecyclerView.Adapter<AlarmItemAdapter.MyViewHolder> {
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View itemList = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.alarm_item_details, parent, false);
+        return new MyViewHolder(itemList);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-
+        holder.name.setText("Dipirona");
+        holder.interval.setText("6");
+        holder.sunRiseTime.setText("06:00");
+        holder.dayTime.setText("12:00");
+        holder.nightTime.setText("18:00");
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 10;
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         private TextView name;
         private TextView interval;
-        private TextView scheduleList;
+        private TextView sunRiseTime;
+        private TextView dayTime;
+        private TextView nightTime;
 
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            name = itemView.findViewById(R.id.medName);
+            interval = itemView.findViewById(R.id.interval);
+            sunRiseTime = itemView.findViewById(R.id.sunRiseTime);
+            dayTime = itemView.findViewById(R.id.dayTime);
+            nightTime = itemView.findViewById(R.id.nightTime);
         }
     }
 }
