077f65dc9beb1440f4e5ef6be6db7f83a0fa958a
alefe <alef.jonatha@gmail.com>
2020-11-30 23:07:21 -0300

    DB Atualizado
    Tabelas usuario, notificação, histórico

--- app/src/main/java/com/probex/medicalerta/database/BancoDadosMed.java
+++ app/src/main/java/com/probex/medicalerta/database/BancoDadosMed.java
@@ -10,6 +10,8 @@ import androidx.annotation.Nullable;
 
 import com.probex.medicalerta.adapter.Alarme;
 import com.probex.medicalerta.adapter.Medicamento;
+import com.probex.medicalerta.adapter.Notificacao;
+import com.probex.medicalerta.adapter.Usuario;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,9 +19,8 @@ import java.util.List;
 public class BancoDadosMed extends SQLiteOpenHelper {
     private static final int VERSAO_BANCO = 1;
     private static final String BANCO_MEDICAMENTO = "db_medicalerta";
-
+    //Tabela Medicamento
     private static final String TABELA_MEDICAMENTO = "tb_medicamento";
-
     private static final String COLUNA_ID_MED = "id_med";
     private static final String COLUNA_INDICACAO = "indicacao";
     private static final String COLUNA_SUBSTANCIA = "substancia";
@@ -31,13 +32,28 @@ public class BancoDadosMed extends SQLiteOpenHelper {
 
     //Tabela alarme
     private static final String TABELA_ALARME = "tb_alarme";
-
     private static final String COLUNA_ID_ALARME = "id_alarme";
-    private static final String COLUNA_ID_MEDE = "id_med";
+    private static final String COLUNA_ID_MEDE = "id_mede";
     private static final String COLUNA_DATA_INICIAL = "data_inicial";
     private static final String COLUNA_DATA_FINAL = "data_final";
     private static final String COLUNA_ULTIMO_ALARME = "ultimo_alarme";
     private static final String COLUNA_INTERVALO = "intervalo";
+
+    //Tabela Usuário
+    private static final String TABELA_USUARIO = "tb_usuario";
+    private static final String COLUNA_ID_USUARIO = "id_usuario";
+    private static final String COLUNA_NOME_USUARIO = "nome_usuario";
+    private static final String COLUNA_IDADE_USUARIO = "idade_usuario";
+    private static final String COLUNA_FOTO_USUARIO = "foto_usuario_";
+
+    //Tabela Notificações
+    private static final String TABELA_NOTIFICACAO = "tb_notificacao";
+    private static final String COLUNA_ID_NOTIFICACAO = "id_notificacao";
+    private static final String COLUNA_NOME_NOTIFICACAO = "nome_notificacao";
+    private static final String COLUNA_DESCRICAO_NOTIFICACAO = "descricao_notificacao";
+
+    //Tabela Histórico
+    private static final String TABELA_HISTORICO = "tb_historico";
 
 
     public BancoDadosMed(@Nullable Context context) {
@@ -67,6 +83,30 @@ public class BancoDadosMed extends SQLiteOpenHelper {
                 + COLUNA_INTERVALO + " INTEGER)";
         db.execSQL(QUERY_COLUNA);
 
+        QUERY_COLUNA = " CREATE TABLE " + TABELA_USUARIO + " ( "
+                + COLUNA_ID_USUARIO + "  INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUNA_NOME_USUARIO + " VARCHAR,"
+                + COLUNA_IDADE_USUARIO + " INTEGER,"
+                + COLUNA_FOTO_USUARIO + " VARCHAR)";
+        db.execSQL(QUERY_COLUNA);
+
+        QUERY_COLUNA = " CREATE TABLE " + TABELA_NOTIFICACAO + " ( "
+                + COLUNA_ID_NOTIFICACAO + "  INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUNA_NOME_NOTIFICACAO + " VARCHAR,"
+                + COLUNA_DESCRICAO_NOTIFICACAO + " TEXT)";
+        db.execSQL(QUERY_COLUNA);
+
+        QUERY_COLUNA = " CREATE TABLE " + TABELA_HISTORICO + " ( "
+                + COLUNA_ID_MED + "  INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUNA_INDICACAO + " INTEGER,"
+                + COLUNA_SUBSTANCIA + " TEXT, "
+                + COLUNA_PRODUTO + " TEXT,"
+                + COLUNA_CONCENTRACAO + " TEXT,"
+                + COLUNA_FORMA_FARMACEUTICA + " TEXT,"
+                + COLUNA_QUANTIDADE + " TEXT,"
+                + COLUNA_SUBCLASSE + " TEXT)";
+        db.execSQL(QUERY_COLUNA);
+
     }
 
     @Override
@@ -74,7 +114,8 @@ public class BancoDadosMed extends SQLiteOpenHelper {
 
     }
 
-    public void addMedicamento(Medicamento medicamento) {
+    //TABELA MEDICAMENTO
+    void addMedicamento(Medicamento medicamento) {
         SQLiteDatabase db = this.getWritableDatabase();
 
         ContentValues values = new ContentValues();
@@ -92,14 +133,14 @@ public class BancoDadosMed extends SQLiteOpenHelper {
         db.close();
     }
 
-    public void apagarMedicamento(Medicamento medicamento) {
+    void apagarMedicamento(Medicamento medicamento) {
         SQLiteDatabase db = this.getWritableDatabase();
 
         db.delete(TABELA_MEDICAMENTO, COLUNA_ID_MED + " = ? ", new String[]{String.valueOf(medicamento.getId_med())});
         db.close();
     }
 
-    public Medicamento selecionarMedicamento(int id_med) {
+    Medicamento selecionarMedicamento(int id_med) {
         SQLiteDatabase db = this.getReadableDatabase();
 
         Cursor cursor = db.query(TABELA_MEDICAMENTO, new String[]{COLUNA_ID_MED, COLUNA_INDICACAO,
@@ -117,7 +158,7 @@ public class BancoDadosMed extends SQLiteOpenHelper {
         return medicamento;
     }
 
-    public void atualizaMedicamento(Medicamento medicamento) {
+    void atualizaMedicamento(Medicamento medicamento) {
         SQLiteDatabase db = this.getWritableDatabase();
 
         ContentValues values = new ContentValues();
@@ -160,9 +201,9 @@ public class BancoDadosMed extends SQLiteOpenHelper {
 
     }
 
-    //ALARME
+    //TABELA ALARME
 
-    public void addAlarme(Alarme alarme) {
+    void addAlarme(Alarme alarme) {
         SQLiteDatabase db = this.getWritableDatabase();
 
         ContentValues values = new ContentValues();
@@ -177,7 +218,7 @@ public class BancoDadosMed extends SQLiteOpenHelper {
         db.close();
     }
 
-    public void apagarAlarme(Alarme alarme) {
+    void apagarAlarme(Alarme alarme) {
         SQLiteDatabase db = this.getWritableDatabase();
 
         db.delete(TABELA_ALARME, COLUNA_ID_ALARME + " = ? ", new String[]{String.valueOf(alarme.getId_alarme())});
@@ -187,8 +228,8 @@ public class BancoDadosMed extends SQLiteOpenHelper {
     Alarme selecionarAlarme(int id_alarme) {
         SQLiteDatabase db = this.getReadableDatabase();
 
-        Cursor cursor = db.query(TABELA_ALARME, new String[]{COLUNA_ID_ALARME, COLUNA_ID_MED, COLUNA_DATA_INICIAL,
-                COLUNA_DATA_FINAL, COLUNA_ULTIMO_ALARME, COLUNA_INTERVALO}, COLUNA_ID_ALARME +
+        Cursor cursor = db.query(TABELA_ALARME, new String[]{COLUNA_ID_ALARME, COLUNA_ID_MEDE, COLUNA_DATA_INICIAL,
+                COLUNA_DATA_FINAL, COLUNA_ULTIMO_ALARME, COLUNA_INTERVALO}, COLUNA_ID_MEDE +
                 " = ? ", new String[]{String.valueOf(id_alarme)}, null, null, null);
 
         if (cursor != null) {
@@ -201,7 +242,7 @@ public class BancoDadosMed extends SQLiteOpenHelper {
         return alarme;
     }
 
-    public void atualizaAlarme(Alarme alarme) {
+    void atualizaAlarme(Alarme alarme) {
         SQLiteDatabase db = this.getWritableDatabase();
 
         ContentValues values = new ContentValues();
@@ -236,6 +277,238 @@ public class BancoDadosMed extends SQLiteOpenHelper {
             } while (c.moveToNext());
         }
         return listaAlarmes;
+
+    }
+
+    //TABELA NOTIFICAÇÃO
+
+    void addMedicamentoHistorico(Medicamento medicamento) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+        values.put(COLUNA_ID_MED, medicamento.getId_med());
+        values.put(COLUNA_INDICACAO, medicamento.getIndicacao());
+        values.put(COLUNA_SUBSTANCIA, medicamento.getSubstancia());
+        values.put(COLUNA_PRODUTO, medicamento.getProduto());
+        values.put(COLUNA_CONCENTRACAO, medicamento.getConcentracao());
+        values.put(COLUNA_FORMA_FARMACEUTICA, medicamento.getForma_farmaceutica());
+        values.put(COLUNA_QUANTIDADE, medicamento.getQuantidade());
+        values.put(COLUNA_SUBCLASSE, medicamento.getSubclasse());
+
+        db.insert(TABELA_HISTORICO, null, values);
+        db.close();
+    }
+
+    void apagarMedicamentoHistorico(Medicamento medicamento) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        db.delete(TABELA_HISTORICO, COLUNA_ID_MED + " = ? ", new String[]{String.valueOf(medicamento.getId_med())});
+        db.close();
+    }
+
+    Medicamento selecionarMedicamentoHistorico(int id_med) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(TABELA_HISTORICO, new String[]{COLUNA_ID_MED, COLUNA_INDICACAO,
+                COLUNA_SUBSTANCIA, COLUNA_PRODUTO, COLUNA_CONCENTRACAO, COLUNA_FORMA_FARMACEUTICA,
+                COLUNA_QUANTIDADE, COLUNA_SUBCLASSE}, COLUNA_ID_MED +
+                " = ? ", new String[]{String.valueOf(id_med)}, null, null, null);
+
+        if (cursor != null) {
+            cursor.moveToFirst();
+        }
+
+        Medicamento medicamento = new Medicamento(Integer.parseInt(cursor.getString(0)),
+                Integer.parseInt(cursor.getString(1)), cursor.getString(2), cursor.getString(3), cursor.getString(4), cursor.getString(5), cursor.getString(6), cursor.getString(7));
+
+        return medicamento;
+    }
+
+    void atualizaMedicamentoHistorico(Medicamento medicamento) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+        values.put(COLUNA_INDICACAO, medicamento.getIndicacao());
+        values.put(COLUNA_SUBSTANCIA, medicamento.getSubstancia());
+        values.put(COLUNA_PRODUTO, medicamento.getProduto());
+        values.put(COLUNA_CONCENTRACAO, medicamento.getConcentracao());
+        values.put(COLUNA_FORMA_FARMACEUTICA, medicamento.getForma_farmaceutica());
+        values.put(COLUNA_QUANTIDADE, medicamento.getQuantidade());
+        values.put(COLUNA_SUBCLASSE, medicamento.getSubclasse());
+
+        db.update(TABELA_HISTORICO, values, COLUNA_ID_MED + " = ?", new String[]{String.valueOf(medicamento.getId_med())});
+    }
+
+    public List<Medicamento> listaTodosMedicamentosHistorico() {
+        List<Medicamento> listaMedicamentos = new ArrayList<Medicamento>();
+
+        String query = "SELECT * FROM " + TABELA_NOTIFICACAO;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor c = db.rawQuery(query, null);
+
+        if (c.moveToFirst()) {
+            do {
+                Medicamento medicamento = new Medicamento();
+                medicamento.setId_med(Integer.parseInt(c.getString(0)));
+                medicamento.setIndicacao(Integer.parseInt(c.getString(1)));
+                medicamento.setSubstancia(c.getString(2));
+                medicamento.setProduto(c.getString(3));
+                medicamento.setConcentracao(c.getString(4));
+                medicamento.setForma_farmaceutica(c.getString(5));
+                medicamento.setQuantidade(c.getString(6));
+                medicamento.setSubclasse(c.getString(7));
+
+                listaMedicamentos.add(medicamento);
+            } while (c.moveToNext());
+        }
+        return listaMedicamentos;
+
+    }
+
+    //TABELA NOTIFICAÇÃO
+    void addNotificação(Notificacao notificacao) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+        values.put(COLUNA_NOME_NOTIFICACAO, notificacao.getNome());
+        values.put(COLUNA_DESCRICAO_NOTIFICACAO, notificacao.getDescricao());
+
+
+        db.insert(TABELA_NOTIFICACAO, null, values);
+        db.close();
+    }
+
+    void apagarNotificacao(Notificacao notificacao) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        db.delete(TABELA_NOTIFICACAO, COLUNA_ID_NOTIFICACAO + " = ? ", new String[]{String.valueOf(notificacao.getId())});
+        db.close();
+    }
+
+    Notificacao selecionarNotificacao(int id_notificacao) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(TABELA_NOTIFICACAO, new String[]{COLUNA_ID_NOTIFICACAO, COLUNA_NOME_NOTIFICACAO, COLUNA_DESCRICAO_NOTIFICACAO}, COLUNA_ID_NOTIFICACAO +
+                " = ? ", new String[]{String.valueOf(id_notificacao)}, null, null, null);
+
+        if (cursor != null) {
+            cursor.moveToFirst();
+        }
+
+        Notificacao notificacao = new Notificacao(Integer.parseInt(cursor.getString(0)),
+                cursor.getString(1), cursor.getString(2));
+
+        return notificacao;
+    }
+
+    void atualizaNotificacao(Notificacao notificacao) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+        values.put(COLUNA_ID_NOTIFICACAO, notificacao.getId());
+        values.put(COLUNA_NOME_NOTIFICACAO, notificacao.getNome());
+        values.put(COLUNA_DESCRICAO_NOTIFICACAO, notificacao.getDescricao());
+
+        db.update(TABELA_NOTIFICACAO, values, COLUNA_ID_NOTIFICACAO + " = ?", new String[]{String.valueOf(notificacao.getId())});
+    }
+
+    public List<Notificacao> listaTodasNotificacao() {
+        List<Notificacao> listaNotificacao = new ArrayList<Notificacao>();
+
+        String query = "SELECT * FROM " + TABELA_NOTIFICACAO;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor c = db.rawQuery(query, null);
+
+        if (c.moveToFirst()) {
+            do {
+                Notificacao notificacao = new Notificacao();
+                notificacao.setId(Integer.parseInt(c.getString(0)));
+                notificacao.setNome(c.getString(1));
+                notificacao.setDescricao(c.getString(2));
+                listaNotificacao.add(notificacao);
+            } while (c.moveToNext());
+        }
+        return listaNotificacao;
+
+    }
+
+    //TABELA Usuario
+
+    void addUsuario(Usuario usuario) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+        values.put(COLUNA_NOME_USUARIO, usuario.getNome());
+        values.put(COLUNA_IDADE_USUARIO, usuario.getIdade());
+        values.put(COLUNA_FOTO_USUARIO, usuario.getFoto());
+
+
+        db.insert(TABELA_USUARIO, null, values);
+        db.close();
+    }
+
+    void apagarUsuario(Usuario usuario) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        db.delete(TABELA_USUARIO, COLUNA_ID_NOTIFICACAO + " = ? ", new String[]{String.valueOf(usuario.getId())});
+        db.close();
+    }
+
+    Usuario selecionarUsuario(int id_usuario) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(TABELA_USUARIO, new String[]{COLUNA_ID_USUARIO, COLUNA_NOME_USUARIO, COLUNA_IDADE_USUARIO, COLUNA_FOTO_USUARIO}, COLUNA_ID_USUARIO +
+                " = ? ", new String[]{String.valueOf(id_usuario)}, null, null, null);
+
+        if (cursor != null) {
+            cursor.moveToFirst();
+        }
+
+        Usuario usuario = new Usuario(Integer.parseInt(cursor.getString(0)),
+                cursor.getString(1), Integer.parseInt(cursor.getString(2)), cursor.getString(3));
+
+        return usuario;
+    }
+
+    void atualizaUsuario(Usuario usuario) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+        values.put(COLUNA_ID_USUARIO, usuario.getId());
+        values.put(COLUNA_NOME_USUARIO, usuario.getNome());
+        values.put(COLUNA_IDADE_USUARIO, usuario.getIdade());
+        values.put(COLUNA_FOTO_USUARIO, usuario.getFoto());
+
+        db.update(TABELA_USUARIO, values, COLUNA_ID_USUARIO + " = ?", new String[]{String.valueOf(usuario.getId())});
+    }
+
+    public List<Usuario> listaTodosUsuarios() {
+        List<Usuario> listaUsuarios = new ArrayList<Usuario>();
+
+        String query = "SELECT * FROM " + TABELA_USUARIO;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor c = db.rawQuery(query, null);
+
+        if (c.moveToFirst()) {
+            do {
+                Usuario usuario = new Usuario();
+                usuario.setId(Integer.parseInt(c.getString(0)));
+                usuario.setNome(c.getString(1));
+                usuario.setIdade(Integer.parseInt(c.getString(2)));
+                usuario.setFoto(c.getString(3));
+                listaUsuarios.add(usuario);
+            } while (c.moveToNext());
+        }
+        return listaUsuarios;
 
     }
 
